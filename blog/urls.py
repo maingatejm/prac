98ac9bf7313f4674cb5b9a6047f4446fed58f51f
maingate_choi <ttbj123@gmail.com>
2016-05-29 16:40:02 +0900

    url settings, admin register, bug fix

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$', 'blog.views.index', name = 'index')
+    url(r'^$', 'blog.views.index', name = 'index'),
+    url(r'^(?P<pk>\d+)/$', 'blog.views.post_detail', name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
-	return render(request, 'blog/index.html')
+	post_list=Post.objects.all()
+	return render(request, 'blog/index.html', {'post_list':post_list,})
 
 def post_detail(request, pk):
 	post = Post.objects.get(pk=pk)
-	return render(request, 'blog/post_detail.html', {'post'=post,})
+	return render(request, 'blog/post_detail.html', {'post':post,})
