7a4ddc994de94d85c1043e46605cc9567e0360ef
maingate_choi <ttbj123@gmail.com>
2016-05-29 16:04:01 +0900

    model modified

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,8 @@ class Post(models.Model):
 	title = models.CharField(max_length=100)
 	photo = models.ImageField()
 	content = models.TextField()
+	def __str__(self):
+		return self.title
 
 class Comment(models.Model):
 	author = models.CharField(max_length=50)
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'blog/index.html')
+
+def post_detail(request, pk):
+	post = Post.objects.get(pk=pk)
+	return render(request, 'blog/post_detail.html', {'post'=post,})
