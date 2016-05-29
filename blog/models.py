6ef080c71bde0cf0563b3bb6f2b0d5be8dc4b7d9
maingate_choi <ttbj123@gmail.com>
2016-05-29 15:23:05 +0900

    model created - Post, Comment

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	photo = models.ImageField()
+	content = models.TextField()
+
+class Comment(models.Model):
+	author = models.CharField(max_length=50)
+	content = models.TextField()
+	created_at = models.DateTimeField(auto_now_add = True)
